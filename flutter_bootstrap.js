2e5a23190926ce223a3ad6890cca10ad5ab6a505
klkim169 <klkim169@users.noreply.github.com>
2026-05-31 17:26:28 +0700

    Reduce mini chart height by 10%: aspectRatio 2.8→3.1, 4.0→4.4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "107000972" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3083616449" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
