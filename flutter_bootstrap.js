1f487dc43737eeeef5c9ec488155ed1327a4d06b
klkim169 <klkim169@users.noreply.github.com>
2026-05-31 16:07:45 +0700

    Fix: replace ListTile with GestureDetector(opaque) for reliable click-to-add on web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "196454594" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2842795702" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
