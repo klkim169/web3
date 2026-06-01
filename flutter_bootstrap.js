f7be86652d97fe031d214d887152a586e53bac77
klkim169 <klkim169@users.noreply.github.com>
2026-06-01 21:03:21 +0700

    Fix mobile layout: responsive AppBar (Expanded search, hide title/time when narrow) + taller 1-col cards

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3278859390" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3966471343" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
